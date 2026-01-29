1f8dfcced37e3479b4d87f162a3c19c2ce1b1424
Shannon Lee <slee@blockstream.com>
2026-01-29 10:00:17 -0800

    mysql bustedness fixed (i think)

--- minibini/settings.py
+++ minibini/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,6 +30,7 @@ ALLOWED_HOSTS = [
     'testserver', 
     'localhost', 
     '127.0.0.1', 
+    'minibini.me',
     '45.33.56.130'
 ]
 
@@ -89,10 +91,10 @@ DATABASES = {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'minibini_db',
-        'USER': 'minibini_user',
-        'PASSWORD': 'dev_password',
-        'HOST': 'localhost',
+        'NAME': os.getenv("DATABASE_NAME"),
+        'USER': os.getenv("DATABASE_USER"),
+        'PASSWORD': os.getenv("DATABASE_PASSWORD"),
+        'HOST': 'mysql',
         'PORT': '3306',
     }
 }
