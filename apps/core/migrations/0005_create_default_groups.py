bb7ed45999e9afb6845fd1e7d96c88b08b3e5e21
Rachel McConnell <doctor@drshiny.com>
2026-03-21 16:38:16 -0700

    fix: add auth and contenttypes migration dependencies

--- apps/core/migrations/0005_create_default_groups.py
+++ apps/core/migrations/0005_create_default_groups.py
@@ -55,6 +55,8 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('core', '0004_alter_user_options'),
+        ('auth', '0012_alter_user_first_name_max_length'),
+        ('contenttypes', '0002_remove_content_type_name'),
     ]
 
     operations = [
