55800b81eb68abd7886df1ab1a2958831782d3c4
Rachel McConnell <doctor@drshiny.com>
2026-03-28 22:42:51 -0700

    feat: add SPA accounting category to QBO account mapping UI

--- frontend/src/routes/SettingsPage.svelte
+++ frontend/src/routes/SettingsPage.svelte
@@ -1,7 +1,10 @@
 <script>
   import QBOConnectionCard from '../components/QBOConnectionCard.svelte';
+  import AccountingCategoryMapping from '../components/AccountingCategoryMapping.svelte';
 </script>
 
 <h2>Settings</h2>
 
 <QBOConnectionCard />
+
+<AccountingCategoryMapping />
